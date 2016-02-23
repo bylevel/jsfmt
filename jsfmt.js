4fe56a6088f83443cd7e2a979ec1b7e2c1259a9a
chronos <xielong2@qq.com>
2016-02-23 14:16:35 +0800

    更新esformatter等相关库版本，调整默认配置文件。不再默认载入jsfmt的配置。

--- jsfmt.js
+++ jsfmt.js
@@ -1,34 +1,34 @@
 'use strict';
 
-var stdin = require('get-stdin');
-var jsfmt = require('jsfmt');
-var extend = require('extend');
+var stdin = require( 'get-stdin' );
+var jsfmt = require( 'jsfmt' );
+var extend = require( 'extend' );
 
-stdin(function(data) {
-    var scope = process.argv[3];
-    var conf = jsfmt.getConfig();
-    var optsJSON = extend({}, conf, JSON.parse(process.argv[4]));
+stdin( function( data ) {
+    var scope = process.argv[ 3 ];
+    var conf = { };
+    var optsJSON = extend( { }, conf, JSON.parse( process.argv[ 4 ] ) );
     var opts = conf;
     var js;
 
     // if we don't have a jsfmtrc file
     // use the settings from .sublime-settings
-    if (! opts.config) {
-        opts = extend({}, opts, JSON.parse(process.argv[2]));
+    if ( !opts.config ) {
+        opts = extend( { }, opts, JSON.parse( process.argv[ 2 ] ) );
     }
 
     try {
 
-        if (scope && scope == 'json') {
-            js = jsfmt.formatJSON(data, optsJSON);
+        if ( scope && scope == 'json' ) {
+            js = jsfmt.formatJSON( data, optsJSON );
         } else {
-            js = jsfmt.format(data, opts);
+            js = jsfmt.format( data, opts );
         }
 
-        process.stdout.write(js);
+        process.stdout.write( js );
 
-    } catch (err) {
+    } catch ( err ) {
         throw err;
     }
-});
+} );
 
